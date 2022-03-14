d8616a1d85a4fbfdef3bdcfc77a5849764978125
Tianshuo <tianshuo@gmail.com>
2022-03-14 21:02:36 +0800

    Fixed semicolons

--- wordgridbot.user.js
+++ wordgridbot.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Find answers for Word Grid Game
+// @name        Word Grid Bot
 // @namespace   https://github.com/tianshuo/wordgridbot
 // @version     0.0.3
 // @author      tianshuo
@@ -10,11 +10,11 @@
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
-
-
+ 
+ 
 (function() {
 const W = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
-
+ 
 var letterPoints={
     "A": 1,
     "B": 3,
@@ -43,7 +43,7 @@ var letterPoints={
     "Y": 4,
     "Z": 10
 };
-
+ 
  function wordPointsCalc(guess ){
      let total = 0;
      let usedLetters = {};
@@ -51,29 +51,29 @@ var letterPoints={
      GUESS.split('').forEach(function(val){
          if(usedLetters[val]){
              total += (letterPoints[val] * usedLetters[val]);
-             usedLetters[val]+=1
+             usedLetters[val]+=1;
          }else{
              total += letterPoints[val];
-             usedLetters[val]=1
+             usedLetters[val]=1;
          }
-     })
+     });
      if(Object.keys(usedLetters).length == 9){
          total += 100;
      }
      return total;
  }
-
-let dict=W.dictionaryImport.map(x=>{return {w:x,s:wordPointsCalc(x)};}) //w: word, s: score
-let sorteddict=dict.sort((x,y)=>x.s-y.s)
+ 
+let dict=W.dictionaryImport.map(x=>{return {w:x,s:wordPointsCalc(x)};}); //w: word, s: score
+let sorteddict=dict.sort((x,y)=>x.s-y.s);
 // Highest score is 'EXPRESSIONLESSNESSES', which comes from `app.letterBoard='eilnoprsx'.toUpperCase().split(“");`
-
-
+ 
+ 
 let dicthash=sorteddict.reduce((acc,i)=>{
-    var k=[...new Set(i.w.split(""))].sort().join("")
+    var k=[...new Set(i.w.split(""))].sort().join("");
     acc[k]=i;
     return acc;
-},{})
-
+},{});
+ 
 function getSubArrays(arr){
   var len = arr.length,
      subs = Array(Math.pow(2,len)).fill();
@@ -87,7 +87,7 @@ function getSubArrays(arr){
                              return res;
                            }).slice(1);
 }
-
+ 
 function findBest(val){
    var chars=val.split("").sort();
    var subchars=getSubArrays(chars);
@@ -95,14 +95,16 @@ function findBest(val){
       if(dicthash[iter.join("")]){
          acc.push(dicthash[iter.join("")]);
       }
-      return acc
-   },[]).sort((x,y)=>y.s-x.s)
+      return acc;
+   },[]).sort((x,y)=>y.s-x.s);
 }
-
-
+ 
+ 
 setInterval(function(){
     if(!W.app.guess){
         W.app.guess=findBest(W.app.letterBoard.join("").toLowerCase())[0].w.toUpperCase();
     }
- },1000)
+ },1000);
+ 
 })();
+ 
